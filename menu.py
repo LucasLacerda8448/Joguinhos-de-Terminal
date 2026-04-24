a8e0939a2faabddcff3a254c1b0e8aec094b1aef
LucasLacerda8448 <106678323+LucasLacerda8448@users.noreply.github.com>
2026-04-24 10:57:15 -0400

    Update menu.py

--- menu.py
+++ menu.py
@@ -4,6 +4,17 @@ import time
 import subprocess
 caminho = ".\\programas\\"
 
+def Escolha(n):
+    e = 0
+    l = []
+    for i in range(1, n+1):
+        l.append(str(i))
+    while e not in l:
+        print("-> ", end="")
+        e = input()
+    print()
+    return e
+
 print("INICIANDO PROGRAMA...")
 print()
 time.sleep(1.3)
@@ -11,14 +22,12 @@ while True:
     print("============== MENU DE JOGOS ==============")
     print("[1] Jogo da Velha       [2] Jogo da Memória")
     print("[3] Jogo da Sequência   [4] UNO!")
-    print("[5] Connect-4           [6] Sair")
+    print("[5] Connect-4           [6] Campo Minado")
+    print("[7] Rolagem de Dados    [8] Sudoku")
+    print("                 [9] Sair")
     print("===========================================")
-    e3 = 0
-    while '1' != e3 != '2' and '3' != e3 != '4' and '5' != e3 != '6':
-        print("-> ", end="")
-        e3 = input()
-    print()
-    if e3 == '6':
+    e3 = Escolha(9)
+    if e3 == '9':
         print("ENCERRANDO PROGRAMA...")
         break
     else:
@@ -35,5 +44,11 @@ while True:
             subprocess.run(["python", caminho + "UNO.py"])
         elif e3 == '5':
             subprocess.run(["python", caminho + "connect.py"])
+        elif e3 == '6':
+            subprocess.run(["python", caminho + "mina.py"])
+        elif e3 == '7':
+            subprocess.run(["python", caminho + "dice.py"])
+        elif e3 == '8':
+            subprocess.run(["python", caminho + "sudoku.py"])    
         print()
         time.sleep(1.2)
